2430b8a3edad162906c4dc2e5854fb632eef8fba
EZHIKprograms <116499591+EZHIKprograms@users.noreply.github.com>
2023-11-26 18:47:13 +0300

    Add files via upload

---
+++ speedtrigger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speedtrigger : MonoBehaviour
+{
+    public float speed = 2.5f;
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        FirstPersonMovement player = other.GetComponent<FirstPersonMovement>();
+        if(player)
+        {
+            player.runSpeed*=speed;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        FirstPersonMovement player = other.GetComponent<FirstPersonMovement>();
+        if(player)
+        {
+            player.runSpeed/=speed;
+        }
+    }
+}
